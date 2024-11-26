3564cc825397027c2ddbcd5d56a5ecc39dd86f0b
Irina Samoylova <novikova.i.v@gmail.com>
2024-11-26 10:30:46 +0300

    Добавлена функция сохранения истории получения ссылок в файл в формате json

--- main_fileio.py
+++ main_fileio.py
@@ -1,8 +1,12 @@
+import os
+import json
 from tkinter import *
 import requests
+import pyperclip #для сохранения в буфер обмена
 from tkinter import filedialog as fd
 from tkinter import ttk
 from tkinter import  messagebox as mb
+
 
 def upload():
     try:
@@ -14,12 +18,28 @@ def upload():
                 response.raise_for_status()
                 data = response.json()
                 link= data['link']
+                #возможность копировать в буфер обмена ссылку
+                pyperclip.copy(link)
                 entry.delete(0,END)
                 entry.insert(0,link)
+                save_history(filepath,link)
+                mb.showinfo("info",'ссылка успешно скопирована в буфер обмена')
     except KeyError:
         mb.showerror('Ошибка','Ссылка не найдена в ответе')
     except Exception as e:
         mb.showerror('Ошибка',f'Ошибка {e}')
+
+def save_history(filepath,link):
+    history = []
+    if os.path.exists(history_file):
+        with open(history_file, 'r') as f:
+            history = json.load(f)
+    history.append({'file_path':os.path.basename(filepath),
+                    'donload_link':link})
+    with open(history_file, 'w') as f:
+        json.dump(history, f,indent=4)
+
+history_file = "upload_history.json"
 
 window = Tk()
 window.title('Отправка файла в облако')
